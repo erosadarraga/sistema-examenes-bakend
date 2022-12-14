f9b7d67d6e7296b5ad6cd6290f2b10d50f109c8f
Eros Adarraga <dev.erosadarraga@gmail.com>
2022-12-14 13:33:57 -0500

    start

---
+++ src/main/java/com/sistema/examenes/entidades/Rol.java
@@ -0,0 +1,52 @@
+package com.sistema.examenes.entidades;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Rol {
+
+    @Id
+    private Long rolId;
+    private String nombre;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "rol")
+    private Set<UsuarioRol> usuariosRoles = new HashSet<>();
+
+    public Long getRolId() {
+        return rolId;
+    }
+
+    public void setRolId(Long rolId) {
+        this.rolId = rolId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Set<UsuarioRol> getUsuariosRoles() {
+        return usuariosRoles;
+    }
+
+    public void setUsuariosRoles(Set<UsuarioRol> usuariosRoles) {
+        this.usuariosRoles = usuariosRoles;
+    }
+
+    public Rol() {
+
+    }
+
+}
