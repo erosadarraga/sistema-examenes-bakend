15fa8e8805c1f2f1b6e8c76204c6bcb0834de2f8
Eros Adarraga <dev.erosadarraga@gmail.com>
2022-12-15 16:57:01 -0500

    cambios

--- src/main/java/com/sistema/examenes/SistemaExamenesBakendApplication.java
+++ src/main/java/com/sistema/examenes/SistemaExamenesBakendApplication.java
@@ -1,13 +1,54 @@
 package com.sistema.examenes;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sistema.examenes.entidades.Rol;
+import com.sistema.examenes.entidades.Usuario;
+import com.sistema.examenes.entidades.UsuarioRol;
+import com.sistema.examenes.servicios.UsuarioService;
+
 @SpringBootApplication
-public class SistemaExamenesBakendApplication {
+public class SistemaExamenesBakendApplication implements CommandLineRunner {
+
+	@Autowired
+	private UsuarioService usuarioService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaExamenesBakendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Usuario usuario = new Usuario();
+
+		usuario.setNombre("Eros");
+		usuario.setEmail("eros@gmail.com");
+		usuario.setTelefone("123456789");
+		usuario.setApellido("adarraga");
+		usuario.setUsername("erosdev");
+		usuario.setPassword("sasa");
+		usuario.setPerfil("FOTO.PNG");
+
+		Rol rol = new Rol();
+		rol.setRolId(1L);
+		rol.setNombre("ADMIN");
+
+		Set<UsuarioRol> usuariosRoles = new HashSet<>();
+
+		UsuarioRol usuarioRol = new UsuarioRol();
+		usuarioRol.setRol(rol);
+		usuarioRol.setUsuario(usuario);
+		usuariosRoles.add(usuarioRol);
+
+		Usuario usuarioGuardado = usuarioService.guardarUsuario(usuario, usuariosRoles);
+		System.out.println(usuarioGuardado.getUsername());
+
 	}
 
 }
